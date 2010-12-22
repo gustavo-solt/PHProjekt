f824af61d92f84c25256801eda5e806e1a85fe03
Gustavo Solt <gustavo.solt@mayflower.de>
2010-12-22 12:17:55 -0300

    Improve tree widget.
    - Remove unused vars and use global vars.
    - Create only one time the tree, the model and the store.
    - Check for changes in the store and add/edit/move the nodes.
    - Improve docs and cc.

--- phprojekt/application/Default/Views/dojo/scripts/system/Tree.js
+++ phprojekt/application/Default/Views/dojo/scripts/system/Tree.js
@@ -26,11 +26,8 @@ phpr.treeLastProjectSelected = null;
 
 dojo.declare("phpr.Tree", phpr.Component, {
     // Summary: This class is responsible for rendering the Tree of a default module
-    _treeNode: null,
-    _url:      null,
-    _idName:   null,
-    _store:    null,
-    _model:    null,
+    url:      null,
+    idName:   null,
 
     constructor:function() {
         this.setUrl();
@@ -38,25 +35,29 @@ dojo.declare("phpr.Tree", phpr.Component, {
     },
 
     loadTree:function() {
-        if (!this._idName || !dijit.byId(this._idName)) {
-            this.setNode();
+        // Summary:
+        //    Init the tree
+        // Description:
+        //    Init the tree if not exists,
+        //    in the other case, select the current project,
+        //    draw the breadcrum and fix the size
+        if (!this.idName) {
             // Data of the tree
-            var _this = this;
-            phpr.DataStore.addStore({url: this._url});
-            phpr.DataStore.requestData({url: this._url, processData: dojo.hitch(this, function() {
-                var content = _this.processData(dojo.clone(phpr.DataStore.getData({url: _this._url})));
-                _this._store = new dojo.data.ItemFileWriteStore({data: content});
-                _this.getModel();
-                _this.tree = _this.getTree();
-                _this.setId(_this.tree.id);
-                _this._treeNode.set('content', _this.tree.domNode);
-                _this.tree.startup();
-                dojo.connect(_this.tree, "onClick", dojo.hitch(this, "onItemClick"));
-                dojo.byId("navigation-container-title").innerHTML = phpr.nls.get('Projects');
-                _this.finishDraw();
+            phpr.DataStore.addStore({url: this.url});
+            phpr.DataStore.requestData({url: this.url, processData: dojo.hitch(this, function() {
+                if (!phpr.Tree.tree) {
+                    phpr.Tree.tree = phpr.Tree.getTree();
+                    phpr.Tree.tree.startup();
+                    phpr.Tree.getNode().set('content', phpr.Tree.tree.domNode);
+                    dojo.connect(phpr.Tree.tree, "onClick", dojo.hitch(this, "onItemClick"));
+                    dojo.byId("navigation-container-title").innerHTML = phpr.nls.get('Projects');
+                } else {
+                    this.processDataDiff();
+                }
+                phpr.Tree.setId(phpr.Tree.tree.id);
+                phpr.Tree.finishDraw();
             })});
         } else {
-            this.tree = dijit.byId(this._idName);
             this.finishDraw();
         }
     },
@@ -72,23 +73,34 @@ dojo.declare("phpr.Tree", phpr.Component, {
     },
 
     getModel:function() {
-        this._model = new dijit.tree.ForestStoreModel({
-            store: this._store,
-            query: {parent:'1'}
+        // Summary:
+        //    Create a new tree model with a new store
+
+        // Create the store
+        var store          = new dojo.data.ItemFileWriteStore({});
+        store.clearOnClose = true;
+        store.data         = this.processData(dojo.clone(phpr.DataStore.getData({url: this.url})));
+
+        // Create the model
+        return new dijit.tree.ForestStoreModel({
+            store: store,
+            query: {parent: '1'}
         });
     },
 
     getTree:function() {
+        // Summary:
+        //    Create a new dijit.tree
         return new dijit.Tree({
-            model:    this._model,
+            model:    this.getModel(),
             showRoot: false,
             persist:  false,
-           _onNodeMouseEnter: function(node) {
-               if (node.item.cut == 'true') {
-                   dijit.showTooltip(node.item.longName, node.domNode);
-               }
-           },
-           _onNodeMouseLeave: function(node) {
+            _onNodeMouseEnter: function(node) {
+                if (node.item.cut == 'true') {
+                    dijit.showTooltip(node.item.longName, node.domNode);
+                }
+            },
+            _onNodeMouseLeave: function(node) {
                 if (node.item.cut == 'true') {
                     dijit.hideTooltip(node.domNode);
                 }
@@ -99,42 +111,35 @@ dojo.declare("phpr.Tree", phpr.Component, {
     getUrl:function() {
         // Summary:
         //    Return the url for get the tree
-        // Description:
-        //    Return the url for get the tree
-        return this._url;
+        return this.url;
     },
 
     setUrl:function() {
         // Summary:
         //    Set the url for get the tree
-        // Description:
-        //    Set the url for get the tree
-        this._url = phpr.webpath + 'index.php/Project/index/jsonTree';
+        this.url = phpr.webpath + 'index.php/Project/index/jsonTree';
     },
 
-    setNode:function() {
+    getNode:function() {
         // Summary:
         //    Set the node to put the tree
-        // Description:
-        //    Set the node to put the tree
-        this._treeNode = dijit.byId("treeBox");
+        return dijit.byId("treeBox");
     },
 
     setId:function(id) {
         // Summary:
         //    Set the id of the widget
-        // Description:
-        //    Set the id of the widget
-        this._idName = id;
+        this.idName = id;
     },
 
     updateData:function() {
-        phpr.destroyWidget(this._idName);
-        phpr.DataStore.deleteData({url: this._url});
+        this.setId(null);
+        phpr.DataStore.deleteData({url: this.url});
     },
 
     onItemClick:function(item) {
-        // Summary: publishes "changeProject" as soon as a tree Node is clicked
+        // Summary:
+        //    Publishes "changeProject" as soon as a tree Node is clicked
         if (!item) {
           item = [];
         }
@@ -143,46 +148,42 @@ dojo.declare("phpr.Tree", phpr.Component, {
 
     selecteCurrent:function(id) {
         // Summary:
-        //    Select the current projects and all the parents
-        // Description:
-        //    Select the current projects and all the parents
-        var _tree = this.tree;
-        var _this = this;
+        //    Select the current projects and open all the parents
+        if (phpr.treeLastProjectSelected != id) {
+            // Remove last bold
+            var node = this.getNodeByidentity(phpr.treeLastProjectSelected);
+            if (node) {
+                dojo.removeClass(node.rowNode, "selected");
+            }
 
-        // Remove last bold
-        var node = _this.getNodeByidentity(phpr.treeLastProjectSelected);
-        if (node) {
-            dojo.removeClass(node.rowNode, "selected");
-        }
-
-        if (id > 1) {
-            // Expan the parents
-            this.tree.model.store.fetchItemByIdentity({identity: id,
-                onItem:function(item) {
-                    if (item) {
-                        var paths = item.path.toString().split("\/");
-                        for (i in paths) {
-                            if (Math.abs(paths[i]) > 1) {
-                                _tree._expandNode(_this.getNodeByidentity(paths[i]));
+            if (id > 1) {
+                // Expan the parents
+                var _tree = this.tree;
+                this.tree.model.store.fetchItemByIdentity({identity: id,
+                    onItem:function(item) {
+                        if (item) {
+                            var paths = item.path.toString().split("\/");
+                            for (var i in paths) {
+                                if (parseInt(paths[i]) > 1) {
+                                    phpr.Tree.tree._expandNode(phpr.Tree.getNodeByidentity(paths[i]));
+                                }
                             }
                         }
-                    }
-            }});
+                }});
 
-            // Add new bold
-            var node = _this.getNodeByidentity(id);
-            if (node) {
-                _tree.focusNode(node);
-                dojo.addClass(node.rowNode, "selected");
-                phpr.treeLastProjectSelected = id;
+                // Add new bold
+                var node = this.getNodeByidentity(id);
+                if (node) {
+                    this.tree.focusNode(node);
+                    dojo.addClass(node.rowNode, "selected");
+                    phpr.treeLastProjectSelected = id;
+                }
             }
         }
     },
 
     getNodeByidentity:function(identity) {
         // Summary:
-        //    Return the node by identity
-        // Description:
         //    Return the node by identity
         var nodes = this.tree._itemNodesMap[identity];
         if (nodes && nodes.length){
@@ -204,7 +205,7 @@ dojo.declare("phpr.Tree", phpr.Component, {
         for(var i in data.items) {
             var name  = data.items[i]['name'].toString();
             var depth = data.items[i]['path'].match(/\//g).length;
-                if (depth > 5) {
+            if (depth > 5) {
                 depth = 5;
             }
             var maxLength = Math.round((width / 11) - (depth - 1));
@@ -223,8 +224,6 @@ dojo.declare("phpr.Tree", phpr.Component, {
     getParentId:function(id) {
         // Summary:
         //    Return the parent id of one project
-        // Description:
-        //    Return the parent id of one project
         if (phpr.treePaths[id]) {
             var paths = phpr.treePaths[id].toString().split("\/").reverse();
             for (i in paths) {
@@ -233,6 +232,7 @@ dojo.declare("phpr.Tree", phpr.Component, {
                 }
             }
         }
+
         return 1;
     },
 
@@ -246,28 +246,28 @@ dojo.declare("phpr.Tree", phpr.Component, {
     },
 
     drawBreadCrumb:function() {
-        var projects = new Array();
-        var _this    = this;
-
+        // Summary:
+        //    Set the Breadcrumb with all the projects and the module
         if (!phpr.isGlobalModule(phpr.module)) {
             if (phpr.treeLastProjectSelected != phpr.currentProjectId || phpr.currentProjectId == 1) {
+                var projects = new Array();
                 this.tree.model.store.fetchItemByIdentity({identity: phpr.currentProjectId,
                     onItem:function(item) {
                         if (item) {
                             var paths = phpr.treePaths[phpr.currentProjectId].toString().split("\/");
-                            for (i in paths) {
+                            for (var i in paths) {
                                 if (paths[i] > 0 && paths[i] != phpr.currentProjectId) {
-                                    _this.tree.model.store.fetchItemByIdentity({identity: paths[i],
+                                    phpr.Tree.tree.model.store.fetchItemByIdentity({identity: paths[i],
                                         onItem:function(item) {
                                             if (item) {
                                                 projects.push({"id":   item.id,
-                                                               "name": item.name});
+                                                               "name": item.longName || item.name});
                                             }
                                     }});
                                 }
                             }
                             projects.push({"id":   item.id,
-                                           "name": item.name});
+                                           "name": item.longName || item.name});
                         }
                 }});
                 phpr.BreadCrumb.setProjects(projects);
@@ -293,5 +293,99 @@ dojo.declare("phpr.Tree", phpr.Component, {
         if (dojo.style("treeBox", "opacity") != 1) {
             dojo.style("treeBox", "opacity", 1);
         }
+    },
+
+    processDataDiff:function() {
+        // Summary:
+        //    Process the new data for the tree
+        // Description:
+        //    Check for changes between the store and the new data.
+        //    - Add new nodes
+        //    - Edit existing nodes
+        //    - Move nodes
+        //    - Delete nodes
+        var data    = this.processData(dojo.clone(phpr.DataStore.getData({url: this.url})));
+        var newData = data.items;
+        this.tree.model.store.fetch({
+            queryOptions: {deep: true},
+            onComplete: dojo.hitch(this, function(oldData) {
+                for (var i = 0; i < oldData.length; i++) {
+                    // Search for a change
+                    var found = false;
+                    for (var j = 0; j < newData.length; j++) {
+                        if (newData[j]['id'] == oldData[i].id) {
+                            if (newData[j]['name'] != oldData[i].name) {
+                                // The name was changed
+                                phpr.Tree.tree.model.store.fetchItemByIdentity({
+                                    identity: newData[j].id,
+                                    onItem:   dojo.hitch(this, function(item) {
+                                        phpr.Tree.tree.model.store.setValue(item, 'name', newData[j].name);
+                                        phpr.Tree.tree.model.store.setValue(item, 'cut', newData[j].cut);
+                                        phpr.Tree.tree.model.store.save({});
+                                    })
+                                });
+                            }
+
+                            if (newData[j]['parent'] != oldData[i].parent) {
+                                // The parent was changed
+                                if (oldData[i].id > 1) {
+                                    phpr.Tree.tree.model.store.fetchItemByIdentity({
+                                        identity: newData[j].id,
+                                        onItem:   dojo.hitch(this, function(item) {
+                                            phpr.Tree.tree.model.store.fetchItemByIdentity({
+                                                identity: oldData[i].parent,
+                                                onItem:   dojo.hitch(this, function(from) {
+                                                    phpr.Tree.tree.model.store.fetchItemByIdentity({
+                                                        identity: newData[j].parent,
+                                                        onItem:   dojo.hitch(this, function(to) {
+                                                            phpr.Tree.tree.model.pasteItem(item, from, to, false);
+                                                            phpr.Tree.tree.model.store.setValue(item,
+                                                                'parent', newData[j].parent);
+                                                            phpr.Tree.tree.model.store.setValue(item,
+                                                                'path', newData[j].path);
+                                                            phpr.Tree.tree.model.store.save({});
+                                                        })
+                                                    });
+                                                })
+                                            });
+                                        })
+                                    });
+                                }
+                            }
+
+                            // Mark as exists
+                            found = true;
+                            newData[j]['exists'] = true;
+                            break;
+                        }
+                    }
+
+                    if (!found) {
+                        // The node don't exists => delete
+                        phpr.Tree.tree.model.store.deleteItem(oldData[i]);
+                        phpr.Tree.tree.model.store.save({});
+                    }
+                }
+
+                // Search for new items
+                for (var j = 0; j < newData.length; j++) {
+                    if (!newData[j]['exists']) {
+                        this.tree.model.store.fetchItemByIdentity({
+                            identity: newData[j].parent,
+                            onItem:   dojo.hitch(this, function(item) {
+				                phpr.Tree.tree.model.newItem({
+				                   id:     newData[j].id,
+					               name:   newData[j].name,
+					               parent: newData[j].parent,
+					               path:   newData[j].path,
+					               cut:    newData[j].cut
+                                }, item);
+                                phpr.Tree.tree.model.store.save({});
+                            })
+                        });
+                    }
+                }
+            })
+        });
     }
 });
